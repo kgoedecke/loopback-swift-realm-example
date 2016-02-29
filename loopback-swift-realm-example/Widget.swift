2821dbeba64255c43cb130c43f0c14ecbb951ac2
Kevin Goedecke <goedecke@net-studios.de>
2016-02-29 01:40:52 +0100

    Major refactoring

--- loopback-swift-realm-example/Widget.swift
+++ loopback-swift-realm-example/Widget.swift
@@ -12,69 +12,65 @@ import LoopBack
 
 class Widget: Object {
     dynamic var name = ""
-    dynamic var updated = NSDate(timeIntervalSince1970: 1)
-    dynamic var remoteId = -1
+    dynamic var updated = NSDate()
+    let remoteId = RealmOptional<Int>()
     
     convenience init(remoteWidget: WidgetRemote) {
         self.init()
         self.name = remoteWidget.name
         self.updated = remoteWidget.updated
-        self.remoteId = remoteWidget._id as! Int
+        self.remoteId.value = remoteWidget._id as? Int
+    }
+    
+    private func updateLocalWidget(remoteWidget: WidgetRemote) {
+        if (self.remoteId.value == remoteWidget._id as? Int || self.remoteId.value == nil) {
+            try! realm!.write    {
+                self.remoteId.value = remoteWidget._id as? Int
+                self.name = remoteWidget.name
+                self.updated = remoteWidget.updated
+                NSLog("Successfully update local widget")
+            }
+        }
+    }
+    
+    private func updateRemoteWidget(remoteWidget: WidgetRemote) {
+        if (remoteWidget._id as? Int == self.remoteId.value || self.remoteId.value == nil) {
+            remoteWidget.name = self.name
+            remoteWidget.updated = self.updated
+            remoteWidget.saveWithSuccess({ () -> Void in
+                self.updateLocalWidget(remoteWidget)
+                NSLog("Successfully update remote widget")
+                }, failure: { (err: NSError!) -> Void in
+                    NSLog(err.description)
+            })
+        }
     }
     
     func syncWithRemote() {
         // Widget was created offline and has never been synced
-        if (self.remoteId == -1) {
-            NSLog("Widget was created offline and has never been synced")
+        if (self.remoteId.value == nil) {
             let widgetRemote = AppDelegate.widgetRepository.modelWithDictionary(nil) as! WidgetRemote
-            widgetRemote.name = self.name
-            widgetRemote.updated = NSDate()
-            widgetRemote.saveWithSuccess({ () -> Void in
-                // Update local object with remote_id, updated and set synced to true
-                let realm = try! Realm()
-                try! realm.write    {
-                    self.remoteId = widgetRemote._id as! Int
-                    self.updated = widgetRemote.updated
-                }
-                }, failure: { (error: NSError!) -> Void in
-                    // Offline, sync not possible
-                    NSLog(error.description)
-            })
+            self.updateRemoteWidget(widgetRemote)
         }
-        // Has a remote_id and thus has already been synced at one point
+        // Has a remoteId and thus has already been synced
         else {
-            // Check if object has been deleted on Remote and if delete locally
-            // For example if findById fails, notice to differenciate between
-            // backend not reachable and id not found
             AppDelegate.widgetRepository.findById(self.remoteId, success: { (model: LBPersistedModel!) -> Void in
-                // Maybe a new remote object with same ID was created in
-                // that case overwrite local widget
                 let remoteWidget = model as! WidgetRemote
-                // Remote is indeed more recent
                 if (remoteWidget.updated.compare(self.updated) != NSComparisonResult.OrderedAscending) {
-                    try! self.realm!.write    {
-                        self.name = remoteWidget.name
-                        self.updated = remoteWidget.updated
-                    }
+                    // Remote is more recent, update local
+                    self.updateLocalWidget(remoteWidget)
                 }
-                // Local is more recent, update remote
                 else {
-                    remoteWidget.name = self.name
-                    remoteWidget.updated = self.updated
-                    remoteWidget.saveWithSuccess({ () -> Void in
-                        NSLog("Successfully update remote widget")
-                        }, failure: { (err: NSError!) -> Void in
-                            NSLog(err.description)
-                    })
+                    // Local is more recent, update remote
+                    self.updateRemoteWidget(remoteWidget)
                 }
                 }, failure: { (err: NSError!) -> Void in
-                    // Error code -1011 means model with id not found
+                    // If object was deleted on Remote (error -1011), then delete locally
                     if (err.code == -1011) {
-                        // Delete all with error code -1011
+                        NSLog("Remote widget not found, deleting local widget")
                         try! self.realm!.write {
                             self.realm!.delete(self)
                         }
-                        NSLog("Remote widget not found, delete local widget")
                     }
             })
         }
